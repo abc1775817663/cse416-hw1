2785e1361da83b835ace9a0bfbd9ced999800a8d
Weng-Sean <143642405+Weng-Sean@users.noreply.github.com>
2023-09-03 19:42:36 -0400

    determine whether a label should be displayed according to the country area

--- src/app/discovery/handleGeoJson.js
+++ src/app/discovery/handleGeoJson.js
@@ -6,19 +6,86 @@ export function handleGeoJson(file)
 
     let xRange = {min: Number.POSITIVE_INFINITY, max: Number.NEGATIVE_INFINITY};
     let yRange = {min: Number.POSITIVE_INFINITY, max: Number.NEGATIVE_INFINITY};
-
-    function drawLabel(display, name, label)
-    {
+    
+    function drawLabel(display, name, label, geometry) {
         let text = document.createElementNS("http://www.w3.org/2000/svg", "text");
         text.textContent = name;
-        text.setAttribute("x", label.x);
-        text.setAttribute("y", -label.y);
-        text.style.fontSize = 12;
+        let fontSize = 12;
+        text.style.fontSize = fontSize;
         text.style.textAnchor = "middle";
         text.style.dominantBaseline = "middle";
-        display.appendChild(text);
+    
+        
+        let area;
+        switch(geometry.type)
+            {
+                case "Polygon":
+                    {
+                        area = calculatePolygonsArea(geometry.coordinates);;
+                    }
+                    break;
+                case "MultiPolygon":
+                    {
+                        area = calculateMultiPolygonsArea(geometry.coordinates);
+                    }
+                    break;
+                default:
+                    alert("Not implemented: geojson/" + geometry.type);
+                    return;
+        }
+        
+        const labelThreshold = fontSize * name.length; 
+    
+        if (area >= labelThreshold) {
+            text.setAttribute("x", label.x);
+            text.setAttribute("y", -label.y);
+            display.appendChild(text);
+        }
+        else{
+            console.log(area, labelThreshold);
+        }
+    }
+    function calculateMultiPolygonsArea(multipolygons){
+        let area = 0;
+        multipolygons.forEach(multipolygon => {
+            area += calculatePolygonsArea(multipolygon);
+        })
+        return area;
+    }
+    
+    function calculatePolygonsArea(polygons){
+        let area = 0;
+        polygons.forEach(polygon => {
+            area += calculatePolygonArea(polygon);
+        })
+        return area;
+    }
+    
+    function calculatePolygonArea(polygonCoordinates) {
+        // shoelace formula
+        let area = 0;
+
+        for (let i = 0; i < polygonCoordinates.length; i++) {
+            let xi = polygonCoordinates[i][0];
+            let yi = polygonCoordinates[i][1];
+            let xi1, yi1;
+    
+            if (i === polygonCoordinates.length - 1) {
+                xi1 = polygonCoordinates[0][0];
+                yi1 = polygonCoordinates[0][1];
+            } else {
+                xi1 = polygonCoordinates[i + 1][0];
+                yi1 = polygonCoordinates[i + 1][1];
+            }
+    
+            area += xi * yi1 - xi1 * yi;
+        }
+    
+
+        return Math.abs(area) / 2;
     }
 
+    
     function drawPolygon(display, polygon)
     {
         let polygonsvg = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
@@ -55,8 +122,6 @@ export function handleGeoJson(file)
             let geometry = obj.features[i].geometry;
             console.log(name);
             console.log(geometry);
-
-            drawLabel(display, name, label);
             switch(geometry.type)
             {
                 case "Polygon":
@@ -79,6 +144,7 @@ export function handleGeoJson(file)
                     alert("Not implemented: geojson/" + geometry.type);
                     return;
             }
+            drawLabel(display, name, label, geometry);
         }
 
         let scale = 0;
