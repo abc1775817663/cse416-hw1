7c1bad624bfbc5a9db46c73e5c2cfecf4a9214d2
ScottZhengGit <Scott.zheng@stonybrook.edu>
2023-09-13 16:12:08 -0400

    Feature: ShapeFile Done

--- src/app/discovery/handleShapefile.js
+++ src/app/discovery/handleShapefile.js
@@ -6,69 +6,100 @@ import shp from "shpjs";
 export default function ShapefileDisplay(props) {
   const mapRef = useRef(null);
   const geoJsonLayerRef = useRef(null);
-  const markers = useRef([]); 
-  
-
-  
+  const countryMarkers = useRef([]);
+  const stateMarkers = useRef([]);
+  const cityMarkers = useRef([]);
 
   useEffect(() => {
     if (!mapRef.current) {
       mapRef.current = L.map("map" + props.mapId).setView([0, 0], 2);
     }
 
-    // Remove old GeoJSON layer if it exists
-
     if (geoJsonLayerRef.current) {
       mapRef.current.removeLayer(geoJsonLayerRef.current);
     }
-    markers.current.forEach((marker) => {
+
+    // Clear existing markers
+    [...countryMarkers.current, ...stateMarkers.current, ...cityMarkers.current].forEach((marker) => {
       mapRef.current.removeLayer(marker);
     });
-    markers.current = [];
+    countryMarkers.current = [];
+    stateMarkers.current = [];
+    cityMarkers.current = [];
 
     const reader = new FileReader();
     reader.onload = async function (event) {
       try {
-        geoJsonLayerRef.current = L.geoJson(
-          { features: [] },
-          {
-            onEachFeature: (feature, layer) => {
-              const label = L.marker(layer.getBounds().getCenter(), {
-                  icon: L.divIcon({
-                    className: 'countryLabel',
-                    html: feature.properties.NAME_2 || feature.properties.NAME_1 || feature.properties.NAME_0,
-                    iconSize: [1000, 0],
-                    iconAnchor: [0, 0]
-                  })
-                }).addTo(mapRef.current);
-                markers.current.push(label);
-              } 
-          }
-        ).addTo(mapRef.current);
-
-
         const data = await shp(event.target.result);
 
         if (data) {
-          geoJsonLayerRef.current.addData(data);
+          geoJsonLayerRef.current = L.geoJson(data, {
+            onEachFeature: (feature, layer) => {
+              if (feature.properties.NAME_2) {
+                const countryLabel = L.marker(layer.getBounds().getCenter(), {
+                  icon: L.divIcon({
+                    className: 'countryLabel',
+                    html: feature.properties.NAME_2,
+                    iconSize: [1000, 0],
+                    iconAnchor: [0, 0]
+                  })
+                });
+                countryMarkers.current.push(countryLabel);
+
+              }
+              else if (feature.properties.NAME_1) {
+                const stateLabel = L.marker(layer.getBounds().getCenter(), {
+                  icon: L.divIcon({
+                    className: 'stateLabel',
+                    html: feature.properties.NAME_1,
+                    iconSize: [1000, 0],
+                    iconAnchor: [0, 0]
+                  })
+                });
+                stateMarkers.current.push(stateLabel);
+              }
+              else if (feature.properties.NAME_0) {
+                const cityLabel = L.marker(layer.getBounds().getCenter(), {
+                  icon: L.divIcon({
+                    className: 'cityLabel',
+                    html: feature.properties.NAME_0,
+                    iconSize: [1000, 0],
+                    iconAnchor: [0, 0]
+                  })
+                });
+                cityMarkers.current.push(cityLabel);
+              }
+            }
+          }).addTo(mapRef.current);
+
+          // Add zoom listener here
+          mapRef.current.on('zoomend', function () {
+            const zoom = mapRef.current.getZoom();
+
+            // Clear all markers first
+            [...countryMarkers.current, ...stateMarkers.current, ...cityMarkers.current].forEach(marker => mapRef.current.removeLayer(marker));
+
+            // Add markers based on zoom level
+            if (zoom > 7) {
+              countryMarkers.current.forEach(marker => marker.addTo(mapRef.current));
+            } else if (zoom > 5) {
+              stateMarkers.current.forEach(marker => marker.addTo(mapRef.current));
+            } else {
+              cityMarkers.current.forEach(marker => marker.addTo(mapRef.current));
+            }
+          });
         } else {
           console.error("Shapefile data is null or invalid.");
         }
-
-        console.log(geoJsonLayerRef.current);
-        console.log(data);
-
       } catch (error) {
         console.error("Error:", error);
       }
     };
+
     reader.readAsArrayBuffer(props.file);
   }, [props.file]);
 
   return (
-    <div
-      id={"map" + props.mapId}
-      style={{ width: "100%", height: "600px" }}
-    ></div>
+    <div id={"map" + props.mapId} style={{ width: "100%", height: "600px" }}></div>
   );
 }
